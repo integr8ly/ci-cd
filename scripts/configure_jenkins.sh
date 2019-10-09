b5a6d7c869de03f8083a5050a5c62266af9656cd
Davide <david3bizzarri@gmail.com>
2019-10-09 15:24:46 +0200

    chore: ad mss and mss operator delorean jobs

--- scripts/configure_jenkins.sh
+++ scripts/configure_jenkins.sh
@@ -62,6 +62,10 @@ generate_inline_script_job $SCRIPTS_DIR/../jobs/delorean/keycloak/ga/branch.yaml
 generate_inline_script_job $SCRIPTS_DIR/../jobs/delorean/keycloak/ga/discovery.yaml
 generate_inline_script_job $SCRIPTS_DIR/../jobs/delorean/mdc-operator/ga/branch.yaml
 generate_inline_script_job $SCRIPTS_DIR/../jobs/delorean/mdc-operator/ga/discovery.yaml
+generate_inline_script_job $SCRIPTS_DIR/../jobs/delorean/mss/ga/branch.yaml
+generate_inline_script_job $SCRIPTS_DIR/../jobs/delorean/mss/ga/discovery.yaml
+generate_inline_script_job $SCRIPTS_DIR/../jobs/delorean/mss-operator/ga/branch.yaml
+generate_inline_script_job $SCRIPTS_DIR/../jobs/delorean/mss-operator/ga/discovery.yaml
 
 #OpenShift Cluster Jobs
 generate_inline_script_job $SCRIPTS_DIR/../jobs/openshift/cluster/create/openshift-cluster-create.yaml
