6f8ed20a17e86a77c70f4847609a8a01d927ba4a
Davide <david3bizzarri@gmail.com>
2019-10-09 16:12:04 +0200

    chore: add delorean jobs for ups and ups operator

--- scripts/configure_jenkins.sh
+++ scripts/configure_jenkins.sh
@@ -62,6 +62,10 @@ generate_inline_script_job $SCRIPTS_DIR/../jobs/delorean/keycloak/ga/branch.yaml
 generate_inline_script_job $SCRIPTS_DIR/../jobs/delorean/keycloak/ga/discovery.yaml
 generate_inline_script_job $SCRIPTS_DIR/../jobs/delorean/mdc-operator/ga/branch.yaml
 generate_inline_script_job $SCRIPTS_DIR/../jobs/delorean/mdc-operator/ga/discovery.yaml
+generate_inline_script_job $SCRIPTS_DIR/../jobs/delorean/ups/ga/branch.yaml
+generate_inline_script_job $SCRIPTS_DIR/../jobs/delorean/ups/ga/discovery.yaml
+generate_inline_script_job $SCRIPTS_DIR/../jobs/delorean/ups-operator/ga/branch.yaml
+generate_inline_script_job $SCRIPTS_DIR/../jobs/delorean/ups-operator/ga/discovery.yaml
 
 #OpenShift Cluster Jobs
 generate_inline_script_job $SCRIPTS_DIR/../jobs/openshift/cluster/create/openshift-cluster-create.yaml
