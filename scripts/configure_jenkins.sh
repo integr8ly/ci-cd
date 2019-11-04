8adfa8be7e69960eb00604abb6b15e6307159f79
Pawel Paszki <ppaszki@redhat.com>
2019-11-04 13:46:30 +0000

    osd OS4 cluster creation pipeline added (#334)

--- scripts/configure_jenkins.sh
+++ scripts/configure_jenkins.sh
@@ -84,8 +84,7 @@ generate_inline_script_job $SCRIPTS_DIR/../jobs/openshift/cluster/integreatly/op
 generate_inline_script_job $SCRIPTS_DIR/../jobs/openshift/cluster/brew/openshift-cluster-brew-image-sync.yaml
 
 # OpenShit 4 Cluster jobs
-jenkins-jobs --conf $CONFIG update $SCRIPTS_DIR/../jobs/openshift4/cluster/create/openshift4-cluster-create.yaml
-jenkins-jobs --conf $CONFIG update $SCRIPTS_DIR/../jobs/openshift4/cluster/deprovision/openshift4-cluster-deprovision.yaml
+generate_inline_script_job $SCRIPTS_DIR/../jobs/integr8ly/ocp4/osd/cluster/create/osd-cluster-create.yaml
 generate_inline_script_job $SCRIPTS_DIR/../jobs/openshift4/cluster/integreatly/openshift4-cluster-integreatly-install.yaml
 
 # OSD cluster jobs
